ca8678b5cd8be6f93dd45d89d98e2d5335f20d2c
Katelyn Luther <katelynluther87@aim.com>
2023-03-06 12:16:00 -0700

    routes rendered

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ function Home() {
     const [activities, setActivities] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3001/activities")
+        fetch("http://localhost:3000/activities")
             .then(res => res.json())
             .then(setActivities)
     },[])
     const renderActivities = activities.map(activity => (
         <div key={activity.id}>
-            <img src={activity.image} />
+            <img src={activity.image} alt=""/>
         </div>
     ))
     return (
