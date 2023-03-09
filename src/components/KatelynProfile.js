a38b16813d56ab0f147f4c30820d568f22e3387a
Katelyn Luther <katelynluther87@aim.com>
2023-03-09 09:08:39 -0700

    added activities/updated profile styling

--- src/components/KatelynProfile.js
+++ src/components/KatelynProfile.js
@@ -7,9 +7,14 @@ function KatelynProfile({activities}) {
         </div>
         ))
         return (
+        <div className="profile">
+            <div className="profile-header">
+                <img className="profile-image" src="https://images2.imgbox.com/3e/f4/MmGkyHGb_o.jpg" style={{height: "100px"}}></img><h2>Katelyn Luther</h2>
+            </div>
             <div className="Container">
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
                 {renderActivitiesByProfile}
+            </div>
             </div>
         </div>
         )
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -4,16 +4,36 @@ function ProfileCard({ activity }) {
         militaryTime = militaryTime.split(':');
         return (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) ? (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.' : militaryTime.join(':') + ' A.M.'
     }
-    
+    function convertMovementTime(time) {
+        const newTime = time.split(":")
+        if(newTime.length === 3) {
+          return(`${newTime[0]}h ${newTime[1]}m ${newTime[2]}s`)
+        }
+        if(newTime.length === 2) {
+          return(`${newTime[0]}m ${newTime[1]}s`)
+        }
+      }
     return (
         <div className="card h-100">
+            <div className="card-header">
+                <div className="runner-header">
+                    <p>{type}</p><b>{title}</b>
+                </div>
+                <div className="date-time">
+                    {date} at {toStandardTime(time)}
+                </div>
+            </div>
             <img className="card-img-top" src={image} alt={title} />
             <div className="card-body">
-                <p className="card-title">{type} {profile}-<b>{title}</b></p>
-                <p>{location}</p>
-                <p>{toStandardTime(time)}</p>
-                <p><b>Movement Time:</b> {movementTime}</p>
-                <p>Distance: {distance} miles</p>
+                <div className="runner-body">
+                    <p><b className="card-font-weight">Movement Time:</b> {convertMovementTime(movementTime)}</p>
+                </div>
+                <div className="runner-body">
+                    <p><b className="card-font-weight">Pace:</b> {pace} {speed}</p>
+                </div>
+                <div className="runner-body align-left">
+                    <p><b className="card-font-weight">Distance:</b> {distance} miles</p>
+                </div>
             </div>
         </div>
     )
